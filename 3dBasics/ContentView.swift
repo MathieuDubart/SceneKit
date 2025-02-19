771fdacae6e67dba1f2f87eb77a6c44edbe35125
Mathieu Dubart <mathieu.dubart@icloud.com>
2025-02-19 16:27:50 +0100

    Initial Commit

---
+++ 3dBasics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3dBasics
+//
+//  Created by Mathieu Dubart on 19/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3dBasics/_dBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _dBasicsApp.swift
+//  3dBasics
+//
+//  Created by Mathieu Dubart on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct _dBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

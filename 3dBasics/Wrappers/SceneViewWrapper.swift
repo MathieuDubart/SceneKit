0a8dc4541329adeb9af8f948a4644560e4be12e0
Mathieu Dubart <mathieu.dubart@icloud.com>
2025-02-19 21:56:19 +0100

    quick refacto

--- 3dBasics/Wrappers/SceneViewWrapper.swift
+++ 3dBasics/Wrappers/SceneViewWrapper.swift
@@ -18,20 +18,29 @@ struct SceneViewWrapper: UIViewRepresentable {
         view.backgroundColor = .black
         view.autoenablesDefaultLighting = true
         
+        let torus = createTorus()
+        view.scene?.rootNode.addChildNode(torus)
+        
+        let action = SCNAction.rotate(by: 90 * (CGFloat.pi/180.0), around: SCNVector3(x: 1, y: 0, z: 0), duration: 3)
+        
+        torus.runAction(action)
+        // torus.removeFromParentNode() to remove it from node
+        return view
+    }
+    
+    func createTorus() -> SCNNode {
         let torus = SCNNode()
         torus.geometry = SCNTorus(ringRadius: 1, pipeRadius: 0.5)
         torus.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
         torus.geometry?.firstMaterial?.specular.contents = UIColor.white
         
-        view.scene?.rootNode.addChildNode(torus)
-        
-        // torus.removeFromParentNode() to rmeove it from node
-        return view
+        return torus
     }
     
     func updateUIView(_ uiView: SCNView, context: Context) {
     }
 }
+
 #Preview {
     SceneViewWrapper()
 }
